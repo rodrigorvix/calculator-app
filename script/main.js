5973778b860fa032f4a7c5e7ac54e2336970f852
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-15 14:39:07 -0300

    update main.js - add localstorage

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 function initCalculator() {
   const buttons = document.querySelectorAll(".buttons-calc button");
   const display = document.querySelector(".display-calc");
-  let blockExpression = '';
+  let blockExpression = "";
 
   buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -10,26 +10,21 @@ function initCalculator() {
       if (buttonTarget === "DEL") {
         display.innerHTML = deleteScreen(display.innerHTML);
         blockExpression = display.innerHTML;
-
       } else if (buttonTarget === "RESET") {
         display.innerHTML = resetScreen(display.innerHTML);
         blockExpression = display.innerHTML;
-
       } else if (buttonTarget === "=") {
         const result = calculator(display.innerHTML);
         display.innerHTML =
           result === "invalid expression" ? result : formatNumber(result);
         blockExpression = display.innerHTML;
-
       } else if (
         (display.innerHTML === "0" && display.innerHTML.length === 1) ||
         display.innerHTML === "invalid expression"
       ) {
         display.innerHTML = button.innerText;
         blockExpression = display.innerHTML;
-
       } else if (buttonTarget === ".") {
-
         const existDot = blockExpression.indexOf(buttonTarget);
 
         if (existDot === -1) {
@@ -45,7 +40,7 @@ function initCalculator() {
         const lastElementDisplay = display.innerHTML.charAt(
           display.innerHTML.length - 1
         );
-        blockExpression = '';
+        blockExpression = "";
 
         if (operations.indexOf(lastElementDisplay) !== -1) {
           display.innerHTML = display.innerHTML.slice(0, -1) + buttonTarget;
@@ -54,7 +49,7 @@ function initCalculator() {
         }
       } else {
         display.innerHTML += button.innerText;
-        blockExpression += button.innerText
+        blockExpression += button.innerText;
       }
     });
   });
@@ -91,49 +86,59 @@ const keysKeyboard = {
   ".": "keyDot",
   Enter: "keyEqual",
 };
-const deleteScreen = displayCurrent => {
+const deleteScreen = (displayCurrent) => {
   if (displayCurrent.length === 1) {
     displayCurrent = "0";
   } else {
     displayCurrent = displayCurrent.slice(0, -1);
   }
   return displayCurrent;
-}
-const resetScreen = displayCurrent => "0";
+};
+const resetScreen = (displayCurrent) => "0";
 
-const calculator = displayCurrent => {
+const calculator = (displayCurrent) => {
   displayCurrent = displayCurrent.replace(/[x]+/g, "*");
   try {
     return eval(displayCurrent);
   } catch (error) {
     return "invalid expression";
   }
-}
-const formatNumber = number => {
+};
+const formatNumber = (number) => {
   if (!Number.isInteger(number)) {
     number = number.toFixed(3);
     number = number.toString().replace(/(^0+(?=\d))|(,?0+$)/g, "");
   }
   return number;
-}
+};
 function themeSelect() {
   const selectOne = document.querySelector("#theme-one");
   const selectTwo = document.querySelector("#theme-two");
   const selectThree = document.querySelector("#theme-three");
   const html = document.querySelector("html");
 
+  html.dataset.theme = localStorage.theme ? localStorage.theme : "one";
+
+  [selectOne, selectTwo, selectThree].forEach((input) => {
+    if (input.getAttribute("id").includes(html.dataset.theme)) {
+      input.click();
+    }
+  });
+
   selectOne.addEventListener("click", () => {
     html.dataset.theme = "one";
+    localStorage.theme = html.dataset.theme;
   });
   selectTwo.addEventListener("click", () => {
     html.dataset.theme = "two";
+    localStorage.theme = html.dataset.theme;
   });
   selectThree.addEventListener("click", () => {
     html.dataset.theme = "three";
+    localStorage.theme = html.dataset.theme;
   });
 }
 
 initCalculator();
 initCalculatorKeybord();
 themeSelect();
-
