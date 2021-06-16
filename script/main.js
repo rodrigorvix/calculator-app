30e04a53a6f4fb8b6f6d01dddb1408082793d53b
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-16 11:34:32 -0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,30 +1,30 @@
 function initCalculator() {
-  const buttons = document.querySelectorAll(".buttons-calc button");
-  const display = document.querySelector(".display-calc");
-  let blockExpression = "";
+  const buttons = document.querySelectorAll('.buttons-calc button');
+  const display = document.querySelector('.display-calc');
+  let blockExpression = '';
 
   buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener('click', (e) => {
       const buttonTarget = e.target.innerText;
 
-      if (buttonTarget === "DEL") {
+      if (buttonTarget === 'DEL') {
         display.innerHTML = deleteScreen(display.innerHTML);
         blockExpression = display.innerHTML;
-      } else if (buttonTarget === "RESET") {
+      } else if (buttonTarget === 'RESET') {
         display.innerHTML = resetScreen(display.innerHTML);
         blockExpression = display.innerHTML;
-      } else if (buttonTarget === "=") {
+      } else if (buttonTarget === '=') {
         const result = calculator(display.innerHTML);
         display.innerHTML =
-          result === "invalid expression" ? result : formatNumber(result);
+          result === 'invalid expression' ? result : formatNumber(result);
         blockExpression = display.innerHTML;
       } else if (
-        (display.innerHTML === "0" && display.innerHTML.length === 1) ||
-        display.innerHTML === "invalid expression"
+        (display.innerHTML === '0' && display.innerHTML.length === 1) ||
+        display.innerHTML === 'invalid expression'
       ) {
         display.innerHTML = button.innerText;
         blockExpression = display.innerHTML;
-      } else if (buttonTarget === ".") {
+      } else if (buttonTarget === '.') {
         const existDot = blockExpression.indexOf(buttonTarget);
 
         if (existDot === -1) {
@@ -32,15 +32,15 @@ function initCalculator() {
           blockExpression += button.innerText;
         }
       } else if (
-        buttonTarget === "+" ||
-        buttonTarget === "-" ||
-        buttonTarget === "x" ||
-        buttonTarget === "/"
+        buttonTarget === '+' ||
+        buttonTarget === '-' ||
+        buttonTarget === 'x' ||
+        buttonTarget === '/'
       ) {
         const lastElementDisplay = display.innerHTML.charAt(
-          display.innerHTML.length - 1
+          display.innerHTML.length - 1,
         );
-        blockExpression = "";
+        blockExpression = '';
 
         if (operations.indexOf(lastElementDisplay) !== -1) {
           display.innerHTML = display.innerHTML.slice(0, -1) + buttonTarget;
@@ -54,10 +54,10 @@ function initCalculator() {
     });
   });
 }
-const operations = ["+", "-", "x", "/"];
+const operations = ['+', '-', 'x', '/'];
 
 function initCalculatorKeybord() {
-  document.addEventListener("keydown", mapKeyboard);
+  document.addEventListener('keydown', mapKeyboard);
 }
 const mapKeyboard = (event) => {
   const key = event.key;
@@ -66,75 +66,74 @@ const mapKeyboard = (event) => {
   }
 };
 const keysKeyboard = {
-  0: "key0",
-  1: "key1",
-  2: "key2",
-  3: "key3",
-  4: "key4",
-  5: "key5",
-  6: "key6",
-  7: "key7",
-  8: "key8",
-  9: "key9",
-  0: "key0",
-  Delete: "keyDel",
-  Backspace: "keyDel",
-  "+": "keySum",
-  "-": "keySubstraction",
-  "*": "keyMultiplication",
-  "/": "keyBar",
-  ".": "keyDot",
-  Enter: "keyEqual",
+  0: 'key0',
+  1: 'key1',
+  2: 'key2',
+  3: 'key3',
+  4: 'key4',
+  5: 'key5',
+  6: 'key6',
+  7: 'key7',
+  8: 'key8',
+  9: 'key9',
+  0: 'key0',
+  Delete: 'keyDel',
+  Backspace: 'keyDel',
+  '+': 'keySum',
+  '-': 'keySubstraction',
+  '*': 'keyMultiplication',
+  '/': 'keyBar',
+  '.': 'keyDot',
+  Enter: 'keyEqual',
 };
 const deleteScreen = (displayCurrent) => {
   if (displayCurrent.length === 1) {
-    displayCurrent = "0";
+    displayCurrent = '0';
   } else {
     displayCurrent = displayCurrent.slice(0, -1);
   }
   return displayCurrent;
 };
-const resetScreen = (displayCurrent) => "0";
+const resetScreen = (displayCurrent) => '0';
 
 const calculator = (displayCurrent) => {
-  displayCurrent = displayCurrent.replace(/[x]+/g, "*");
+  displayCurrent = displayCurrent.replace(/[x]+/g, '*');
   try {
     return eval(displayCurrent);
   } catch (error) {
-    return "invalid expression";
+    return 'invalid expression';
   }
 };
 const formatNumber = (number) => {
   if (!Number.isInteger(number)) {
     number = number.toFixed(3);
-    number = number.toString().replace(/(^0+(?=\d))|(,?0+$)/g, "");
+    number = number.toString().replace(/(^0+(?=\d))|(,?0+$)/g, '');
   }
   return number;
 };
 function themeSelect() {
-  const selectOne = document.querySelector("#theme-one");
-  const selectTwo = document.querySelector("#theme-two");
-  const selectThree = document.querySelector("#theme-three");
-  const html = document.querySelector("html");
+  const selectOne = document.querySelector('#theme-one');
+  const selectTwo = document.querySelector('#theme-two');
+  const selectThree = document.querySelector('#theme-three');
+  const html = document.querySelector('html');
 
-  html.dataset.theme = localStorage.theme ? localStorage.theme : "one";
+  html.dataset.theme = localStorage.theme ? localStorage.theme : 'one';
 
   [selectOne, selectTwo, selectThree].forEach((input) => {
-    if (input.getAttribute("id").includes(html.dataset.theme)) {
+    if (input.getAttribute('id').includes(html.dataset.theme)) {
       input.click();
     }
   });
-
-  selectOne.addEventListener("click", () => {
-    html.dataset.theme = "one";
+  selectOne.addEventListener('click', () => {
+    html.dataset.theme = 'one';
     localStorage.theme = html.dataset.theme;
   });
-  selectTwo.addEventListener("click", () => {
-    html.dataset.theme = "two";
+  selectTwo.addEventListener('click', () => {
+    html.dataset.theme = 'two';
     localStorage.theme = html.dataset.theme;
   });
-  selectThree.addEventListener("click", () => {
-    html.dataset.theme = "three";
+  selectThree.addEventListener('click', () => {
+    html.dataset.theme = 'three';
     localStorage.theme = html.dataset.theme;
   });
 }
